e59c6775da39a72e0242ddba7be5391c9849bed3
johnmendez <john.a.mendez2012@gmail.com>
2017-02-21 15:25:38 -0600

    Finished for Now

--- app/index.js
+++ app/index.js
@@ -1,1 +1,21 @@
 import 'whatwg-fetch';
+import fillResults from './fill-in-result'
+
+const searchBtn = document.querySelector('.netflix-submit');
+const searchInput = document.querySelector('.netflix-video');
+const resultsCard = document.querySelector('.card');
+// const searchForm = document.querySelector('.search-bar');
+
+function searchForShow(name) {
+  fetch(`http://netflixroulette.net/api/api.php?title=${name}`)
+    .then(r => r.json())
+    .then((data) => {
+      fillResults(resultsCard, data);
+    });
+}
+
+searchForShow('Attack%20on%20titan');
+
+searchBtn.addEventListener('click', () => {
+  searchForShow(searchInput.value);
+});
